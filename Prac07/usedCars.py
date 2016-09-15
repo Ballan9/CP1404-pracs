4bbe126940af0e02d5c396da58a54146e9f52dd4
Ballan9 <allan.serrurier@my.jcu.edu.au>
2016-09-15 11:51:57 +1000

    add car and used car examples for prac

---
+++ Prac07/usedCars.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 Practical
+Client code to use the Car class
+Note that the import has a folder (module) in it.
+"""
+from Prac07.car import Car
+
+
+def main():
+    bus = Car("bus", 180)
+    bus.drive(30)
+    limo = Car("limo", 100)
+    limo.add_fuel(20)
+    limo.drive(115)
+    print(bus)
+    print(limo)
+
+main()
